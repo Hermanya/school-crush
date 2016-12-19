6301ecc9e9d023806641087ca03975025cdd0dcf
Herman Starikov <herman.starikov@uxpsystems.com>
2016-12-18 21:10:00 -0500

    try to fix sw offline

--- publish-to-gh-pages.sh
+++ publish-to-gh-pages.sh
@@ -3,7 +3,8 @@ git branch -D gh-pages
 git push origin :gh-pages
 git checkout -b gh-pages
 polymer build
-sed -i -e 's/<link rel="import" href="shared-bundle.html">//g' build/bundled/index.html
+sed -i -e 's%<link rel="import" href="shared-bundle.html">%%g' build/bundled/index.html
+sed -i -e 's%/index.html%/school-crush/index.html%g' build/bundled/service-worker.js
 cp -r build/bundled/* .
 git add .
 git commit -m 'update gh-pages'
