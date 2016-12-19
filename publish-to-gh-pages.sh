aba4bbbd2672a33b57274ff27d9fe2f9dddde68f
Herman Starikov <herman.starikov@uxpsystems.com>
2016-12-18 20:07:33 -0500

    update styles

--- publish-to-gh-pages.sh
+++ publish-to-gh-pages.sh
@@ -3,6 +3,7 @@ git branch -D gh-pages
 git push origin :gh-pages
 git checkout -b gh-pages
 polymer build
+sed -i -e 's/<link rel="import" href="shared-bundle.html">//g' build/bundled/index.html
 cp -r build/bundled/* .
 git add .
 git commit -m 'update gh-pages'
